ddde7e7f0faf3bc17262bbe5ebf6cf3163bb8862
Leo <gabuevleo991@gmail.com>
2026-03-24 23:08:03 +0300

    Рефакторинг экрана профиля на верстку кодом

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,103 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image=UIImage(named: "PhotoProfile")
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 23)
+        label.text = "Екатерина Новикова"
+        label.textColor = .ypWhiteIOS
+        return label
+    }()
+    
+    private let loginNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.text = "@ekaterina_novikova"
+        label.textColor = .ypWhiteIOS
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.text = "Hello, world!"
+        label.textColor = .ypWhiteIOS
+        return label
+    }()
+    
+    private let logoutButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "ExitButton")
+        button.tintColor = .ypRedIOS
+        button.setImage(image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    override func viewDidLoad() {
+        
+        view.backgroundColor = .ypBlackIOS
+        
+        setupView()
+        setupConstraints()
+        
+        
+        
+        super.viewDidLoad()
+    }
+    
+    private func setupView() {
+        view.addSubview(avatarImageView)
+        view.addSubview(nameLabel)
+        view.addSubview(loginNameLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(logoutButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+            avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            loginNameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: loginNameLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: loginNameLabel.trailingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+        ])
+        
+    }
+    @objc private func logoutTapped() {
+        //
+    }
+    
+}
