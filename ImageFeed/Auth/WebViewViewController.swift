cea4457200dce2883633956b76d30149eb53c5de
Leo <gabuevleo991@gmail.com>
2026-05-07 21:57:51 +0300

    no message

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -24,7 +24,7 @@ final class WebViewViewController: UIViewController {
     
     private lazy var backButton: UIButton = {
         let button = UIButton(type: .custom)
-        button.setImage(UIImage(named: "nav_back_button"), for: .normal)
+        button.setImage(UIImage(named: "backButton"), for: .normal)
         button.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
